1db8ff1809d410e69289aee7230697cb6338d3ec
yangfan_hailv <fan.yang@elestyle.jp>
2025-02-24 01:31:04 +0900

    nothing

--- functions/[[path]].js
+++ functions/[[path]].js
@@ -1,4 +1,14 @@
 // functions/[[path]].js
 export async function onRequest(context) {
-	return fetch(context.request);
+	const { request } = context;
+	const url = new URL(request.url);
+
+	if (url.pathname.startsWith('/testvitepress')) {
+		const newPath = url.pathname.replace(/^\/testvitepress/, '');
+		const newUrl = new URL(newPath, 'https://testvitepress.pages.dev');
+		return fetch(newUrl, request);
+	}
+
+	// 注意：这里不再直接转发，而是让 Pages 处理
+	return context.next();
 }
